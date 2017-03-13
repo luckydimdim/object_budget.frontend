62774461c66cfef89a892fd01fc298539ef359f0
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-03-13 17:14:58 +0300

    remove js grid

--- lib/src/object_budget_component.dart
+++ lib/src/object_budget_component.dart
@@ -1,14 +1,8 @@
 import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
-import 'dart:async';
-import 'dart:html';
 import 'package:alert/alert_service.dart';
 
 import 'package:resources_loader/resources_loader.dart';
-
-import 'package:grid/JsObjectConverter.dart';
-import 'package:grid/jq_grid.dart';
-
 @Component(selector: 'object-budget')
 @View(
     templateUrl: 'object_budget_component.html',
@@ -29,140 +23,14 @@ class ObjectBudgetComponent
   ObjectBudgetComponent(this._router, this._alertService, this._resourcesLoaderService) {}
 
   void breadcrumbInit(){
-    var  breadcrumbContent = querySelector('#breadcrumbContent') as HtmlElement;
 
-    if (breadcrumbContent == null)
-      return;
-
-    breadcrumbContent.innerHtml = '''
-            <li class="breadcrumb-item"><a href="#/master/dashboard">Главная</a></li>
-            <li class="breadcrumb-item"><a href="#/master/objectBudgetList">Список бюджетов по объектам строительства</a></li>
-            <li class="breadcrumb-item active">Бюджет по объекту</li>
-    ''';
   }
 
   @override
   void ngOnInit() {
     breadcrumbInit();
-
-    showGrid("#grid", 'packages/object_budget/src/months_budget.json');
   }
 
   @override
   void ngOnDestroy() {}
-
-  Future showGrid(String selector, url) async {
-    var columns = new List<Column>();
-
-    columns.add(new Column()
-      ..dataField = 'Name'
-      ..text = 'Наименование этапа/работы'
-      ..width = '400px'
-      ..pinned = true);
-
-    var monthColumnWidth = '100px';
-
-    columns.add(new Column()
-      ..dataField = '01_2017'
-      ..text = 'Январь'
-      ..columnGroup = '2017'
-      ..width = monthColumnWidth);
-
-    columns.add(new Column()
-      ..dataField = '02_2017'
-      ..text = 'Февраль'
-      ..columnGroup = '2017'
-      ..width = monthColumnWidth);
-
-    columns.add(new Column()
-      ..dataField = '03_2017'
-      ..text = 'Март'
-      ..columnGroup = '2017'
-      ..width = monthColumnWidth);
-
-    columns.add(new Column()
-      ..dataField = '04_2017'
-      ..text = 'Апрель'
-      ..columnGroup = '2017'
-      ..width = monthColumnWidth);
-
-    columns.add(new Column()
-      ..dataField = '05_2017'
-      ..text = 'Май'
-      ..columnGroup = '2017'
-      ..width = monthColumnWidth);
-
-    columns.add(new Column()
-      ..dataField = '06_2017'
-      ..text = 'Июнь'
-      ..columnGroup = '2017'
-      ..width = monthColumnWidth);
-
-    columns.add(new Column()
-      ..dataField = '07_2017'
-      ..text = 'Июль'
-      ..columnGroup = '2017'
-      ..width = monthColumnWidth);
-
-    columns.add(new Column()
-      ..dataField = '08_2017'
-      ..text = 'Август'
-      ..columnGroup = '2017'
-      ..width = monthColumnWidth);
-
-    columns.add(new Column()
-      ..dataField = '09_2017'
-      ..text = 'Сентябрь'
-      ..columnGroup = '2017'
-      ..width = monthColumnWidth);
-
-    columns.add(new Column()
-      ..dataField = '10_2017'
-      ..text = 'Октябрь'
-      ..columnGroup = '2017'
-      ..width = monthColumnWidth);
-
-    columns.add(new Column()
-      ..dataField = '11_2017'
-      ..text = 'Ноябрь'
-      ..columnGroup = '2017'
-      ..width = monthColumnWidth);
-
-    columns.add(new Column()
-      ..dataField = '12_2017'
-      ..text = 'Декабрь'
-      ..columnGroup = '2017'
-      ..width = monthColumnWidth);
-
-    var groups = new List<ColumnGroup>();
-
-    groups.add(new ColumnGroup()
-      ..name = '2017'
-      ..text = '2017'
-      ..align = 'center');
-    groups.add(new ColumnGroup()
-      ..name = '2017'
-      ..text = '2018'
-      ..align = 'center');
-
-    var hierarchy = new Hierarchy()
-      ..root = 'children';
-
-    var source = new SourceOptions()
-      ..url = url
-      ..id = 'recid'
-      ..hierarchy = hierarchy
-      ..dataType = 'json';
-
-    var options = new GridOptions()
-      ..checkboxes = false
-      ..source = source
-      ..columnGroups = groups
-      ..columns = columns;
-
-    var _worksGrid = new jqGrid(this._resourcesLoaderService, selector,
-        JsObjectConverter.convert(options));
-
-    await _worksGrid.Init();
-  }
 }
